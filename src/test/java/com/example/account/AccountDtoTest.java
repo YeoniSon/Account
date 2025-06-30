489b8f7033d0f5664c0e80ebc0341f183795bb6d
YeoniSon <sjy8211@gmail.com>
2025-07-01 00:33:26 +0900

    lombok의 기능 중 getter, setter 실습

---
+++ src/test/java/com/example/account/AccountDtoTest.java
@@ -0,0 +1,21 @@
+package com.example.account;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDtoTest {
+
+    @Test
+    void accountDto(){
+        //given
+        //when
+        //then
+
+        AccountDto accountDto = new AccountDto();
+        accountDto.setAccountNumber("accountNumber");
+
+        System.out.println(accountDto.getAccountNumber());
+    }
+
+}
