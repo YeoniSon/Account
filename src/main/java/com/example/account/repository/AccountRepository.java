1f8fcb642587dd2faba914ac42249fc754eb30fa
YeoniSon <sjy8211@gmail.com>
2025-07-06 00:15:09 +0900

    계좌 생성 api 생성 -6 유저당 10개가 max로 정하는 코드 작성

--- src/main/java/com/example/account/repository/AccountRepository.java
+++ src/main/java/com/example/account/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.example.account.repository;
 
 import com.example.account.domain.Account;
+import com.example.account.domain.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 //jpaRepository를 이용하여 값을 가져오는 것
 
     Optional<Account> findFirstByOrderByIdDesc();
+
+    Integer countByAccountUser(AccountUser accountUser); // accountUser가 여러개를 받을 수 있어서 가능
+
 }
